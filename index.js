776d43785905c8c7d2c590f1bad5ab67b2d112e4
Holly Teunis <+>
2021-10-18 20:30:29 -0400

    commit tree 504834e2c69efbc0c61273e87ce070bbbf110e89

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-// Code your solution here
+const findMatching = (arr, query) => {
+  return arr.filter(el => el.toLowerCase().indexOf(query.toLowerCase()) !== -1)
+}
+
+function fuzzyMatch(arr, query) {
+  return arr.filter(function(el) {
+    return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  })
+}
